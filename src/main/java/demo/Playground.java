89926b6058f1c155c4efd469fbd5a413f59fb468
TreasureAD <treasuredavis111@gmail.com>
2024-08-30 08:30:56 -0400

    Are we there yet code

--- src/main/java/demo/Playground.java
+++ src/main/java/demo/Playground.java
@@ -1,9 +1,33 @@
+//Treasure Davis
+//Java Homework
+
 package demo;
+
+import java.util.Scanner;
 
 public class Playground {
     public static void main(String[] args) {
+        //while vs for loop
+        //write a method that keeps scanning or asking a user a question such as are we there yet?
+        //when the use says yes..then quit the program
+        System.out.println("Are we there yet?");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();  // Read user input
 
-        System.out.println("Treasure");
+        while(answer.equals("no")){
+
+                //if answer is no ask the question again
+                System.out.println("Answer is : " + answer);  // Output user input
+                System.out.println("Are we there yet?");
+                scanner = new Scanner(System.in);
+                answer = scanner.nextLine();  // Read user input
+
+                if(answer.equals("yes")){
+                    //what i want to do if the answer is yes
+                    System.out.println("Finally!");
+                }
+
+        }
 
     }
 }
